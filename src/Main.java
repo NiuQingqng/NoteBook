520861f611ec30cf1c76027cd5828a3f470faeda
niuniu <niuniu>
2023-05-04 20:34:23 +0800

    初始化代码

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+package src;
+
+/**
+ * 测试主类
+ *
+ * @author Luyu
+ * @date 2023/5/4
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
